9a47ff1293ddb6b522cb7e76e6962c8d47bfb7af
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-07-31 20:34:38 +0800

    [BE][Easy][18/19] enforce style for empty lines in import segments in `torch/d*/`
    ghstack-source-id: 5fee3de3411c48ee91c0c23bfc42da32f30f0d2b
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/129770

--- tools/linter/adapters/ufmt_linter.py
+++ tools/linter/adapters/ufmt_linter.py
@@ -55,7 +55,6 @@ ISORT_SKIPLIST = re.compile(
                     # torch/[a-c]*/**
                     "torch/[a-c]*/**",
                     # torch/d*/**
-                    "torch/d*/**",
                     # torch/[e-n]*/**
                     "torch/[e-n]*/**",
                     # torch/[o-z]*/**
