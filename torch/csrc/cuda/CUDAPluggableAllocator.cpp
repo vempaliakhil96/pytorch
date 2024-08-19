f3ace0cf6af58625798adeb4589170a9ffc2b741
Yu, Guangye <guangye.yu@intel.com>
2024-08-19 15:40:57 +0000

    Refactor caching device allocator utils
    ghstack-source-id: c9334b372c13e139c9491fa3818903c4dea0e576
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/130923

--- torch/csrc/cuda/CUDAPluggableAllocator.cpp
+++ torch/csrc/cuda/CUDAPluggableAllocator.cpp
@@ -210,7 +210,7 @@ void CUDAPluggableAllocator::recordStream(
   }
 }
 
-c10::cuda::CUDACachingAllocator::DeviceStats CUDAPluggableAllocator::
+c10::DeviceStats CUDAPluggableAllocator::
     getDeviceStats(c10::DeviceIndex device) {
   TORCH_CHECK(
       false,
