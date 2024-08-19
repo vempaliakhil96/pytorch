942b95bb97875bdba915a67f4a32f5f000cfb105
yanbing-j <yanbing.jiang@intel.com>
2024-08-19 13:37:48 +0800

    Keep is_mkldnn, add is_onednn Update ConvBackend
    ghstack-source-id: 4fc369d919fadd816c920350229b281a6e1d564f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/133419

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -520,7 +520,7 @@ def _reduce_tensor(
     PyCodeCache would need more complexity to create a new module from its
     cache, but with the right constants attached as attributes.
     """
-    if t.is_mkldnn:
+    if t.is_onednn:
         # TODO: These tensors don't currently pickle, so we can't cache a
         # compiled graph containing them. Just fail now. If mkldnn tensors
         # get pickling support, we can remove this.
@@ -1711,7 +1711,7 @@ class AotCodeCompiler:
             output_o = os.path.splitext(input_path)[0] + ".o"
             consts_size = sum(
                 torch.ops.onednn._nbytes(tensor)
-                if tensor.is_mkldnn
+                if tensor.is_onednn
                 else tensor.untyped_storage().nbytes()
                 for (name, tensor) in graph.constants.items()
                 if name not in graph.folded_constants
@@ -1791,7 +1791,7 @@ class AotCodeCompiler:
                 if t.numel() == 0:
                     return b""
 
-                if t.is_mkldnn:
+                if t.is_onednn:
                     data_ptr = torch.ops.onednn.data_ptr(t)
                     nbytes = torch.ops.onednn._nbytes(t)
                 else:
