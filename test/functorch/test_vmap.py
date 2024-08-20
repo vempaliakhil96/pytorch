95ae1b48ab7f4046b56ad831e4df93b97362804d
chilli <chilli@meta.com>
2024-08-20 15:10:36 -0700

    Added batching rule for sdpa_math and sdpa_efficient_attention forward
    ghstack-source-id: 12def9ae50622256632913b8ff53d7540b8408af
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/133964

--- test/functorch/test_vmap.py
+++ test/functorch/test_vmap.py
@@ -3850,6 +3850,67 @@ class TestVmapBatchedGradient(Namespace.TestVmapBase):
         x = torch.randn(2, 3, device=device, requires_grad=True)
         self._batched_grad_test(lambda x: F.threshold(x, 0.5, 0.0), (x,))
 
+    def test_sdpa(self, device):
+        if device == "cpu":
+            raise unittest.SkipTest("This test is only for CUDA for now")
+        from torch.nn.attention import sdpa_kernel, SDPBackend
+
+        backends = [
+            SDPBackend.MATH,
+            SDPBackend.EFFICIENT_ATTENTION,
+            SDPBackend.FLASH_ATTENTION,
+        ]
+
+        def T(*args):
+            return torch.randn(*args, dtype=torch.float16, device=device)
+
+        for backend in backends:
+            backend_ctx = sdpa_kernel([backend])
+            with backend_ctx:
+                for batching in [
+                    (True, True, True),
+                    (True, False, False),
+                    (False, True, True),
+                ]:
+                    size = [8, 4, 128, 64]
+                    if batching[0]:
+                        query = T(3, *size)
+                    else:
+                        query = T(*size)
+                    if batching[1]:
+                        key = T(3, *size)
+                    else:
+                        key = T(*size)
+                    if batching[2]:
+                        value = T(3, *size)
+                    else:
+                        value = T(*size)
+                    in_dims = tuple(0 if b else None for b in batching)
+                    attention = F.scaled_dot_product_attention
+
+                    self._vmap_test(
+                        attention,
+                        (query, key, value),
+                        in_dims=in_dims,
+                    )
+                    # Backwards test doesn't work yet
+                    # self._batched_grad_test(
+                    #     lambda query, key, value: F.scaled_dot_product_attention(
+                    #         query, key, value
+                    #     ),
+                    #     (query, key, value),
+                    # )
+
+                B = 4
+                query = torch.rand(4, 32, B, 8, 128, dtype=torch.float16, device=device)
+                key = torch.rand(4, B, 32, 8, 128, dtype=torch.float16, device=device)
+                value = torch.rand(4, 32, 8, 128, dtype=torch.float16, device=device)
+                self._vmap_test(
+                    F.scaled_dot_product_attention,
+                    (query, key, value),
+                    in_dims=(2, 1, None),
+                )
+
     @allowVmapFallbackUsage
     def test_inplace_view(self, device):
         leaf = torch.randn(4, 5, requires_grad=True)
